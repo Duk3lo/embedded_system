3455e74487777c3e2334f48c17ab674f3f3835f7
Duk3lo <jhosue10avecillas@gmail.com>
2026-05-11 23:38:55 -0500

    esp32

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
 fn main() {
-    // Lee el archivo .env si existe
     if let Ok(it) = dotenvy::dotenv_iter() {
         for item in it {
             let (key, value) = item.expect("Error leyendo variable del .env");
-            // Esto le dice a Cargo que cree una variable de entorno para el compilador
             println!("cargo:rustc-env={}={}", key, value);
         }
     }
-    
-    // Configuración necesaria para ESP-IDF
     embuild::espidf::sysenv::output();
 }
